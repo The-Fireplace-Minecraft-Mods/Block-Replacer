0cc66edcd354cc08d1578803e960bcca18b62a2d
The_Fireplace <the.f1repl4ce@gmail.com>
2018-10-27 16:47:35 -0500

    2.0 update

--- src/main/java/the_fireplace/wgblockreplacer/WGBlockReplacer.java
+++ src/main/java/the_fireplace/wgblockreplacer/WGBlockReplacer.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import the_fireplace.wgblockreplacer.proxy.Common;
 
-@Mod(modid = WGBlockReplacer.MODID, name = WGBlockReplacer.MODNAME, guiFactory = "the_fireplace.wgblockreplacer.config.WGBRGuiFactory", canBeDeactivated = true, updateJSON = "http://thefireplace.bitnamiapp.com/jsons/wgblockreplacer.json", acceptedMinecraftVersions = "[1.12,1.13)", acceptableRemoteVersions = "*")
+@Mod(modid = WGBlockReplacer.MODID, name = WGBlockReplacer.MODNAME, guiFactory = "the_fireplace.wgblockreplacer.config.WGBRGuiFactory", canBeDeactivated = true, acceptedMinecraftVersions = "[1.12,1.13)", acceptableRemoteVersions = "*")
 public class WGBlockReplacer {
 	public static final String MODID = "wgblockreplacer";
 	public static final String MODNAME = "WorldGen Block Replacer";
@@ -31,28 +31,42 @@ public class WGBlockReplacer {
 	public static class ConfigValues{
 		@Config.Comment("The block id to replace.")
 		@Config.LangKey("replaceblock")
-		public static String replaceblock = "minecraft:stone";
+		public static String[] replaceblock = {"minecraft:stone"};
 		@Config.Comment(("The block meta to replace. Use -1 for the block's default state."))
 		@Config.LangKey("replaceblockmeta")
-		public static int replaceblockmeta = -1;
+		public static int[] replaceblockmeta = {-1};
 		@Config.Comment("The block id to replace the block with.")
 		@Config.LangKey("replacewith")
-		public static String replacewith = "minecraft:stone";
+		public static String[] replacewith = {"minecraft:stone"};
 		@Config.Comment("The block meta for the replacement block. Use -1 for the block's default state.")
 		@Config.LangKey("replacewithmeta")
-		public static int replacewithmeta = -1;
+		public static int replacewithmeta[] = {-1};
 		@Config.Comment("Enables using blocks that might crash/lag the game if used to replace other blocks. Enable at your own risk.")
 		@Config.LangKey("riskyblocks")
 		public static boolean riskyblocks = false;
 		@Config.Comment("This is the Dimension Black/Whitelist. If it contains *, it is a blacklist. Otherwise, it is a whitelist.")
 		@Config.LangKey("dimension_list")
-		public static String[] dimension_list = new String[]{"*"};
+		public static String[] dimension_list = {"*"};
 		@Config.Comment("What percentage of the blocks get replaced. 0.0D = 0%, 1.0D = 100%")
 		@Config.RangeDouble(min = 0.0D, max=1.0D)
 		@Config.LangKey("replacepercent")
-		public static double replacepercent = 1.0D;
+		public static double[] replacepercent = {1.0D};
 		@Config.Comment("Multiplies the block removal chance by the block's y-value.")
 		@Config.LangKey("multiplychance")
-		public static boolean multiplychance = false;
+		public static boolean[] multiplychance = {false};
+		@Config.Comment("The lowest Y value the block should be replaced at")
+		@Config.RangeInt(min=-1,max=256)
+		@Config.LangKey("miny")
+		public static int[] miny = {-1};
+		@Config.Comment("The highest Y value the block should be replaced at")
+		@Config.RangeInt(min=-1,max=256)
+		@Config.LangKey("maxy")
+		public static int[] maxy = {256};
+		@Config.Comment("This is the Biome Black/Whitelist. If it contains *, it is a blacklist. Otherwise, it is a whitelist.")
+		@Config.LangKey("biomefilter")
+		public static String[] biomefilter = {"*"};
+		@Config.Comment("Increase the precision of the biome filter. This may reduce performance.")
+		@Config.LangKey("biomeprecision")
+		public static boolean biomeprecision = true;
 	}
 }
--- src/main/java/the_fireplace/wgblockreplacer/events/CommonEvents.java
+++ src/main/java/the_fireplace/wgblockreplacer/events/CommonEvents.java
@@ -3,15 +3,16 @@ package the_fireplace.wgblockreplacer.events;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
-import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.commons.lang3.ArrayUtils;
-import org.apache.logging.log4j.Level;
 import the_fireplace.wgblockreplacer.WGBlockReplacer;
 import static the_fireplace.wgblockreplacer.WGBlockReplacer.ConfigValues.*;
 
@@ -24,86 +25,126 @@ public class CommonEvents {
 
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void onEvent(PopulateChunkEvent.Pre event) {
-		boolean doEvent = ArrayUtils.contains(dimension_list, "*");
-		for (String dim : dimension_list)
-				try{
-					if(event.getWorld().provider.getDimension() == Integer.parseInt(dim)) {
+		if(displayWarning && (replaceblock.length != replacewith.length || replacewith.length != replacewithmeta.length || replacewithmeta.length != replaceblockmeta.length || replaceblockmeta.length != replacepercent.length || replacepercent.length != dimension_list.length || dimension_list.length != multiplychance.length || multiplychance.length != miny.length || miny.length != maxy.length || maxy.length != biomefilter.length)){
+			System.err.println("[WorldGen Block Replacer] Array lengths do not match!");
+			displayWarning = false;
+			return;
+		}
+		for(int i=0;i<replaceblock.length;i++) {
+			boolean doEvent = ArrayUtils.contains(dimension_list[i].split(","), "*");
+			for (String dim : dimension_list[i].split(","))
+				try {
+					if (event.getWorld().provider.getDimension() == Integer.parseInt(dim)) {
 						doEvent = !doEvent;
 						break;
 					}
-				}catch(NumberFormatException e){
-					if(!dim.equals("*") && event.getWorld().provider.getDimensionType().getName().toLowerCase().equals(dim.toLowerCase())){
+				} catch (NumberFormatException e) {
+					if (!dim.equals("*") && event.getWorld().provider.getDimensionType().getName().toLowerCase().equals(dim.toLowerCase())) {
 						doEvent = !doEvent;
 						break;
 					}
 				}
-		if(!doEvent)
-			return;
+			if (!doEvent)
+				continue;
 
-		Chunk chunk = event.getWorld().getChunkFromChunkCoords(event.getChunkX(), event.getChunkZ());
+			Chunk chunk = event.getWorld().getChunkFromChunkCoords(event.getChunkX(), event.getChunkZ());
 
-		Block toBlock = Block.getBlockFromName(replacewith);
-		Block fromBlock = Block.getBlockFromName(replaceblock);
-		if(toBlock == fromBlock && replaceblockmeta == replacewithmeta)
-			return;
-		if(fromBlock == null) {
-			if(displayWarning) {
-				FMLLog.log(Level.ERROR, "[WorldGen Block Replacer] WorldGen Block Replacer is improperly configured. The block to replace was not found.");
-				displayWarning = false;
-			}
-			return;
-		}
-		if(toBlock == null) {
-			if(displayWarning) {
-				FMLLog.log(Level.ERROR, "[WorldGen Block Replacer] WorldGen Block Replacer is improperly configured. The block to replace with was not found.");
-				displayWarning = false;
-			}
-			return;
-		}
-		if(displayWarning && replaceblockmeta < -1 || replaceblockmeta > 15) {
-			FMLLog.log(Level.ERROR, "[WorldGen Block Replacer] WorldGen Block Replacer might be improperly configured. The block meta is out of the standard range.");
-			displayWarning = false;
-		}
-		if(displayWarning && replacewithmeta < -1 || replacewithmeta > 15) {
-			FMLLog.log(Level.ERROR, "[WorldGen Block Replacer] WorldGen Block Replacer might be improperly configured. The replacement block meta is out of the standard range.");
-			displayWarning = false;
-		}
-		if(!riskyblocks && WGBlockReplacer.isBlockRisky(toBlock)) {
-			if(displayWarning) {
-				FMLLog.log(Level.ERROR, "[WorldGen Block Replacer] WorldGen Block Replacer is configured not to use risky blocks, but you have specified a risky block to be used. Defaulting to Stone instead.");
-				displayWarning = false;
-			}
-			toBlock = Blocks.STONE;
-		}
-
-		IBlockState fromState;
-		if(replaceblockmeta == -1)
-			fromState = fromBlock.getDefaultState();
-		else
-			fromState = fromBlock.getStateFromMeta(replaceblockmeta);
-
-		IBlockState toState;
-		if(replacewithmeta == -1)
-			toState = toBlock.getDefaultState();
-		else
-			toState = toBlock.getStateFromMeta(replacewithmeta);
-
-		int chunkNum = 0;
-		for (ExtendedBlockStorage storage : chunk.getBlockStorageArray()) {
-			if (storage != null) {
-				for (int x = 0; x < 16; x++) {
-					for (int y = 0; y < 16; y++) {
-						for (int z = 0; z < 16; z++) {
-							if (storage.get(x, y, z).equals(fromState)) {
-								if(rand.nextDouble()*(multiplychance?(chunkNum*16+y):1) <= replacepercent)
-									storage.set(x, y, z, toState);
-							}
+			if(!biomeprecision) {
+				doEvent = ArrayUtils.contains(biomefilter[i].split(","), "*");
+				for (String biome : biomefilter[i].split(","))
+					try {
+						if (chunk.getBiome(new BlockPos(chunk.getPos().getXStart(), 64, chunk.getPos().getZStart()), event.getWorld().getBiomeProvider()) == Biome.REGISTRY.getObject(new ResourceLocation(biome))) {
+							doEvent = !doEvent;
+							break;
+						}
+					} catch (Exception e) {
+						if (!biome.equals("*")) {
+							System.err.println("[WorldGen Block Replacer] WorldGen Block Replacer is improperly configured. The biome (" + biome + ") was not found.");
+							displayWarning = false;
 						}
 					}
-				}
+				if (!doEvent)
+					continue;
 			}
-			chunkNum++;
+
+			Block toBlock = Block.getBlockFromName(replacewith[i]);
+			Block fromBlock = Block.getBlockFromName(replaceblock[i]);
+			if (toBlock == fromBlock && replaceblockmeta == replacewithmeta)
+				continue;
+			if (fromBlock == null) {
+				if (displayWarning) {
+					System.err.println("[WorldGen Block Replacer] WorldGen Block Replacer is improperly configured. The block to replace ("+i+") was not found.");
+					displayWarning = false;
+				}
+				continue;
+			}
+			if (toBlock == null) {
+				if (displayWarning) {
+					System.err.println("[WorldGen Block Replacer] WorldGen Block Replacer is improperly configured. The block to replace ("+i+") with was not found.");
+					displayWarning = false;
+				}
+				continue;
+			}
+			if (displayWarning && replaceblockmeta[i] < -1 || replaceblockmeta[i] > 15) {
+				System.err.println("[WorldGen Block Replacer] WorldGen Block Replacer might be improperly configured. The block meta ("+i+") is out of the standard range.");
+				displayWarning = false;
+			}
+			if (displayWarning && replacewithmeta[i] < -1 || replacewithmeta[i] > 15) {
+				System.err.println("[WorldGen Block Replacer] WorldGen Block Replacer might be improperly configured. The replacement block meta ("+i+") is out of the standard range.");
+				displayWarning = false;
+			}
+			if (!riskyblocks && WGBlockReplacer.isBlockRisky(toBlock)) {
+				if (displayWarning) {
+					System.err.println("[WorldGen Block Replacer] WorldGen Block Replacer is configured not to use risky blocks, but you have specified a risky block to be used ("+i+"). Defaulting to Stone instead.");
+					displayWarning = false;
+				}
+				toBlock = Blocks.STONE;
+			}
+
+
+
+			IBlockState fromState;
+			if (replaceblockmeta[i] == -1)
+				fromState = fromBlock.getDefaultState();
+			else
+				fromState = fromBlock.getStateFromMeta(replaceblockmeta[i]);
+
+			IBlockState toState;
+			if (replacewithmeta[i] == -1)
+				toState = toBlock.getDefaultState();
+			else
+				toState = toBlock.getStateFromMeta(replacewithmeta[i]);
+
+			int chunkNum = 0;
+			for (ExtendedBlockStorage storage : chunk.getBlockStorageArray()) {
+				if (storage != null)
+					for (int x = 0; x < 16; x++)
+						for (int y = 0; y < 16; y++)
+							for (int z = 0; z < 16; z++)
+								if (storage.get(x, y, z).equals(fromState))
+									if (miny[i] <= (chunkNum * 16 + y) && maxy[i] >= (chunkNum * 16 + y) && rand.nextDouble() * (multiplychance[i] ? (chunkNum * 16 + y) : 1) <= replacepercent[i]) {
+										if(biomeprecision) {
+											doEvent = ArrayUtils.contains(biomefilter[i].split(","), "*");
+											for (String biome : biomefilter[i].split(","))
+												try {
+													if (chunk.getBiome(new BlockPos(x, y, z), event.getWorld().getBiomeProvider()) == Biome.REGISTRY.getObject(new ResourceLocation(biome))) {
+														doEvent = !doEvent;
+														break;
+													}
+												} catch (Exception e) {
+													if (!biome.equals("*")) {
+														System.err.println("[WorldGen Block Replacer] WorldGen Block Replacer is improperly configured. The biome (" + biome + ") was not found.");
+														displayWarning = false;
+													}
+												}
+											if (doEvent)
+												storage.set(x, y, z, toState);
+										} else
+											storage.set(x, y, z, toState);
+									}
+				chunkNum++;
+			}
+			chunk.markDirty();
 		}
-		chunk.setModified(true);
 	}
 }
